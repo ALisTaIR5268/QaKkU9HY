d59695f0fc4d2bf1c99c55d13de6b508c567a57d
ALisTaIR5268 <sphere_triad0p@icloud.com>
2025-07-08 14:45:41 +0200

    Update the file's environment variables for better security

--- 6x-/SmLM/5n3t/uI4Mzo/end-to-end-testing_zZBl7_1747224694.data.js
+++ 6x-/SmLM/5n3t/uI4Mzo/end-to-end-testing_zZBl7_1747224694.data.js
@@ -76,7 +76,6 @@ module.exports = {
         object: 'os',
         property: 'userInfo',
         message: 'os.userInfo() will throw when there is not an `/etc/passwd` entry for the current user (like when running with --user 12345 in Docker). Do not use it unless you catch any potential errors.',
-      },
     ],
     'no-restricted-syntax': [
       // esquery tool: https://estools.github.io/esquery/
